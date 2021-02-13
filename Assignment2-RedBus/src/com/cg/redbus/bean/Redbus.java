00c9fc98418261059fab7f0ba6b41df86e6b95d1
Vinosh S <Vinosh S@Vinosh>
2021-02-13 22:27:15 +0530

    Comment

--- Assignment2-RedBus/src/com/cg/redbus/bean/Redbus.java
+++ Assignment2-RedBus/src/com/cg/redbus/bean/Redbus.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//Assignment
 public class Redbus {
 	
 	public static void main(String[] args) throws InterruptedException {
